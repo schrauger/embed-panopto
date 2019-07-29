b7cb5ca6296b97f9ae93c39ce03dd32c4dbb18fb
Stephen Schrauger <stephen@schrauger.com>
2019-07-29 14:10:04 -0400

    server-side rendering to allow iframe for non-privileged user

--- panopto-block.js
+++ panopto-block.js
@@ -1,4 +1,4 @@
-wp.blocks.registerBlockType('brad/border-box', {
+wp.blocks.registerBlockType('panopto-embed/panopto-block', {
     title: 'Panopto Playlist',
     icon: 'playlist-video',
     category: 'embed',
@@ -8,10 +8,22 @@ wp.blocks.registerBlockType('brad/border-box', {
     },
 
     edit: function(props) {
+
+        /**
+         * Save playlist id in state. Unset the submitted flag.
+         * @param event
+         */
         function updatePlaylistID(event) {
-            props.setAttributes({playlistID: event.target.value});
+            props.setAttributes({
+                playlistID: event.target.value,
+                submitted: false // if the user edits the pid, unsubmit it - that way, they can edit it and not constantly see render errors
+            });
         }
 
+        /**
+         * Set the submitted flag, as long as the playlistID exists.
+         * @param event
+         */
         function formSubmit(event){
             console.log('submitting');
             event.preventDefault(); // don't actually submit the form
@@ -21,60 +33,73 @@ wp.blocks.registerBlockType('brad/border-box', {
         }
 
         return (
-            (!(props.attributes.playlistID && props.attributes.submitted)) ? ( // if user has not submitted the id (or id is null), render the form. else, render the iframe inside the block
+             // if user has not submitted the id (or id is null), render the form. else, render the iframe inside the block
+            React.createElement(
+                "form",
+                {
+                    onSubmit: formSubmit
+                },
                 React.createElement(
-                    "form",
+                    "h3",
+                    null,
+                    "Panopto Playlist Embed"
+                ),
+                React.createElement(
+                    "label",
                     {
-                        onSubmit: formSubmit
+                        for: 'panoptoPlaylistID'
                     },
-                    React.createElement(
-                        "h3",
-                        null,
-                        "Panopto Playlist Embed"
-                    ),
-                    React.createElement(
-                        "label",
-                        {
-                            for: 'panoptoPlaylistID'
-                        },
-                        "Playlist ID (pid)"
-                    ),
-                    React.createElement(
-                        "input",
-                        {
-                            type: "string",
-                            value: props.attributes.playlistID,
-                            onChange: updatePlaylistID,
-                            name: "panoptoPlaylistID"
-                        }
-                    ),
-                    React.createElement(
-                        "input",
-                        {
-                            type: "submit",
-                            value: "Embed",
-                            className: "components-button is-button is-default is-large"
-                        }
-                    ),
-                )
-            ): ( // after they click submit, remove the form elements and render the iframe inside the block.
-            // note: the user must delete the block and re-add it if they want to embed a different playlist.
+                    "Playlist ID (pid)"
+                ),
                 React.createElement(
-                    "iframe",
+                    "input",
                     {
-                        src: "https://ucf.hosted.panopto.com/Panopto/Pages/Embed.aspx?pid=" + props.attributes.playlistID + "&v=1",
-                        width: 720,
-                        height: 405,
-                        frameborder: 0,
-                        allowfullscreen: true,
-                        allow: 'autoplay'
+                        type: "string",
+                        value: props.attributes.playlistID,
+                        onChange: updatePlaylistID,
+                        name: "panoptoPlaylistID"
                     }
+                ),
+                React.createElement(
+                    "input",
+                    {
+                        type: "submit",
+                        value: "Embed",
+                        className: "components-button is-button is-default is-large"
+                    }
+                ),
+
+                ((props.attributes.playlistID && props.attributes.submitted)) ? (// after they click submit, remove the form elements and render the iframe inside the block.
+                // note: the user must delete the block and re-add it if they want to embed a different playlist.
+                    React.createElement(
+                        "iframe",
+                        {
+                            src: "https://ucf.hosted.panopto.com/Panopto/Pages/Embed.aspx?pid=" + props.attributes.playlistID + "&v=1",
+                            width: 720,
+                            height: 405,
+                            frameborder: 0,
+                            allowfullscreen: true,
+                            allow: 'autoplay'
+                        }
+                    )
+                ) : (
+                    React.createElement(
+                        "div",
+                        null,
+                        "Enter a playlist ID and click embed"
+                    )
                 )
             )
         )
     },
 
     save: function(props) {
+        // this can simply return 'null', which tells wordpress to just save the input attributes.
+        // however, by actually saving the html, this saves the html in the database as well, which means
+        // that our plugin can be disabled and the old pages will still have iframe html. however, if an unprivileged
+        // user edits that page, the iframe code will be stripped out upon saving.
+        // due to the html filtering, this return is not strictly used, as the server-side render method overwrites
+        // this when printing onto the page (but that allows us to print out raw html without filtering, regardless of user).
         return (props.attributes.playlistID && props.attributes.submitted) ? // only create the iframe if playlistID is defined and form is submitted
             (
                 React.createElement(
--- panopto-embed.php
+++ panopto-embed.php
@@ -4,19 +4,57 @@
 Plugin Name: Panopto Playlist Embed
 Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
 Description: WordPress Block for embedding Panopto playlists.
-Version: 1.3.2
+Version: 1.5
 Author: Stephen Schrauger
 Author URI: https://github.com/schrauger/wp-panopto-embed
 License: A "Slug" license name e.g. GPL2
 */
 
+add_action('init', 'loadPanoptoBlock');
+
+/**
+ * register the react script for a block, and also define
+ * the server side render callback to allow for raw html.
+ */
 function loadPanoptoBlock() {
-	wp_enqueue_script(
+
+	wp_register_script(
 		'panopto-block',
-		plugin_dir_url(__FILE__) . 'panopto-block.js',
-		array('wp-blocks','wp-editor'),
-		filemtime(plugin_dir_path(__FILE__) . 'panopto-block.js')
+		plugins_url('panopto-block.js', __FILE__),
+		array('wp-blocks','wp-editor','wp-data')
+		//filemtime(plugin_dir_path(__FILE__) . 'panopto-block.js')
+	);
+
+	register_block_type(
+		'panopto-embed/panopto-block', array(
+			'editor_script' => 'panopto-block',
+			'render_callback' => 'renderPanoptoCallback' // lets us do server-side rendering
+		)
 	);
 }
 
-add_action('enqueue_block_editor_assets', 'loadPanoptoBlock');
+/**
+ * Overwrites the client side 'save' method with our own data. This allows us to print out raw html without
+ * filtering it based on user permissions, so we can embed an iframe.
+ * @param $attributes // input elements from the client
+ * @param $content // post-filtered html from the client-side 'save' method. we don't use it here, instead we create our own html.
+ *
+ * @return string // like shortcode callbacks, this is the html that we render in place of the block.
+ */
+function renderPanoptoCallback($attributes, $content){
+	return "
+		<iframe 
+			src='https://ucf.hosted.panopto.com/Panopto/Pages/Embed.aspx?pid={$attributes['playlistID']}&v=1',
+			width: 720,
+            height: 405,
+            frameborder: 0,
+            allowfullscreen: true,
+            allow: 'autoplay'
+		/>
+	";
+}
+
+// have to use init hook, since there is server-side rendering
+// can't just use enqueue_block_editor_assets hook, since an editor-only return is client side rendered html, which is
+// subject to html filtering (and would remove the iframe for less privileged users)
+//add_action('enqueue_block_editor_assets', 'loadPanoptoBlock');
